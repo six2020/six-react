0164cbcb7c0291340895e75a5051a1e9953382f9
sieyes <1512168128@qq.com>
2021-04-27 22:06:16 +0800

    07

---
+++ 07-react/demo/src/App.js
@@ -0,0 +1,104 @@
+import React, {Fragment} from 'react'
+import PropTypes from 'prop-types'
+import './App.css';
+
+
+// JSX
+/**
+ *  JavaScript and XML 可拓展的标记语言
+ * 
+ * 
+ * 
+*/
+
+// ------------------------------------------
+
+// 类组件 CC
+
+class MyComponent extends React.Component{
+  render(){
+    console.log(`this ====>>>>`, this);
+
+    let {name, age} = this.props;
+    
+    return (
+      <>
+        <p>hello world！！</p>
+        <p>name: {name}</p>
+        <p>age: {age}</p>
+      </>
+    )
+  }
+}
+
+// ------------------------------------------
+
+// 函数组件 MyFc
+function MyFc(props){
+  // console.log(`props ====>>>>`, props);
+  
+  return (
+    <div>
+      函数组件
+
+      <ul>
+        <li>
+          name: {props.name}
+        </li>
+        <li>
+          age: {props.age + 1 }
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+MyFc.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number
+}
+
+// ------------------------------------------
+
+let divstyle = {
+  color: 'red', 
+  fontSize: '50px'
+}
+
+
+let prop = {
+  name: 'sixx',
+  age: 16,
+  bb: 'b',
+  cc: 'c'
+}
+
+// 函数组件 FC
+function App() {
+
+  let name = 'sieyes'
+
+  return (
+
+
+    <>
+      <div className="App" title='lalalal' tabIndex='1' >
+        hello world
+        <div style={divstyle}>div</div>
+      </div>
+
+      <p>1111111 {name} </p>
+
+      <hr />
+
+      {/* 这是类组件 */}
+      <MyComponent  {...prop} />
+
+      <hr />
+
+      <MyFc name='sieyes' age={12} aa='abc' />
+    </>
+  );
+}
+
+export default App;
---
+++ 07-react/demo/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// React.createElement('h1', {title: 'h1标签'}, React.createElement('p', null, 'hfksjdhf'))
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
